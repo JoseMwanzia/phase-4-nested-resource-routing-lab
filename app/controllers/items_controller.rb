3c6cad3c5e435d680c2fa6c176bb10c99515f4ac
joseph mwanzia <makamwanzia@gmail.com>
2023-03-24 04:03:39 +0300

    Completed lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
+   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  #  GET /users/:user_id/items
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  # GET /users/:user_id/items/:id
+  def show 
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  # POST /users/:user_id/items
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.create(items_params)
+    end
+      render json: item, status: :created
+  end
+
+  private
+  #  error not-found method 
+  def render_not_found_response
+    render json: {error: "User not found"}, status: :not_found
+  end
+
+  # items-params method
+  def items_params
+    params.permit(:name, :description, :price)
+  end
 end
